2910503e761e07e01e8123991344daee0433bafc
drakeet <drakeet.me@gmail.com>
2017-10-15 22:00:34 +0800

    Added Recommended for Pure Mosiac

--- sample/src/main/java/me/drakeet/support/about/sample/AboutActivity.java
+++ sample/src/main/java/me/drakeet/support/about/sample/AboutActivity.java
@@ -55,13 +55,23 @@ public class AboutActivity extends AbsAboutActivity {
 
         items.add(new Category("应用推荐"));
         items.add(new Recommended(
-            0, "纯纯写作",
+            0, getString(R.string.pure_writer),
             "https://storage.recommend.wetolink.com/storage/app_recommend/images/YBMHN6SRpZeF0VHbPZWZGWJ2GyB6uaPx.png",
             "com.drakeet.purewriter",
-            "这是一个快速的纯文本编辑器，我们希望写作能够回到原本的样子：纯粹、有安全感、随时、绝对不丢失内容、具备良好的写作体验。",
+            "快速的纯文本编辑器，我们希望写作能够回到原本的样子：纯粹、有安全感、随时、绝对不丢失内容、具备良好的写作体验。",
             "https://www.coolapk.com/apk/com.drakeet.purewriter",
             "2017-10-09 16:46:57",
             "2017-10-09 16:46:57", null, 2.93)
+        );
+        items.add(new Line());
+        items.add(new Recommended(
+            1, getString(R.string.pure_mosaic),
+            "http://image.coolapk.com/apk_logo/2016/0831/ic_pure_mosaic-2-for-16599-o_1argff2ddgvt1lfv1b3mk2vd6pq-uid-435200.png",
+            "me.drakeet.puremosaic",
+            "专注打码的轻应用，包含功能：传统马赛克、毛玻璃效果、选区和手指模式打码，更有创新型高亮打码和 LowPoly 风格马赛克。只为满足一个纯纯的打码需求，让打码也能成为一种赏心悦目。",
+            "https://www.coolapk.com/apk/me.drakeet.puremosaic",
+            "2017-10-09 16:46:57",
+            "2017-10-09 16:46:57", null, 2.64)
         );
 
         items.add(new Category("Open Source Licenses"));
