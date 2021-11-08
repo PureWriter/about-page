f3cd737908eb1384f407e59b8e6d9ca30686cc0b
Drakeet <drakeet@drakeet.com>
2021-11-08 14:07:44 +0800

    Update AboutActivity.java

--- sample/src/main/java/com/drakeet/about/sample/AboutActivity.java
+++ sample/src/main/java/com/drakeet/about/sample/AboutActivity.java
@@ -55,26 +55,26 @@ public class AboutActivity extends AbsAboutActivity
     items.add(new Card(getString(R.string.card_content)));
 
     items.add(new Category("Developers"));
-    items.add(new Contributor(R.drawable.avatar_drakeet, "drakeet", "Developer & designer", "http://weibo.com/drak11t"));
+    items.add(new Contributor(R.drawable.avatar_drakeet, "Drakeet", "Developer & designer", "http://weibo.com/drak11t"));
     items.add(new Contributor(R.drawable.avatar_drakeet, "黑猫酱", "Developer", "https://drakeet.me"));
     items.add(new Contributor(R.drawable.avatar_drakeet, "小艾大人", "Developer"));
 
     items.add(new Category("我独立开发的应用"));
     items.add(new Recommendation(
         0, getString(R.string.pure_writer),
-        "https://storage.recommend.wetolink.com/storage/app_recommend/images/YBMHN6SRpZeF0VHbPZWZGWJ2GyB6uaPx.png",
+        "https://raw.githubusercontent.com/PureWriter/about-page/master/images/app_writer.png",
         "com.drakeet.purewriter",
         "快速的纯文本编辑器，我们希望写作能够回到原本的样子：纯粹、有安全感、随时、绝对不丢失内容、具备良好的写作体验。",
-        "https://www.coolapk.com/apk/com.drakeet.purewriter",
+        "https://play.google.com/store/apps/details?id=com.drakeet.purewriter",
         "2017-10-09 16:46:57",
         "2017-10-09 16:46:57", 2.93, true)
     );
     items.add(new Recommendation(
         1, getString(R.string.pure_mosaic),
-        "http://image.coolapk.com/apk_logo/2016/0831/ic_pure_mosaic-2-for-16599-o_1argff2ddgvt1lfv1b3mk2vd6pq-uid-435200.png",
+        "https://raw.githubusercontent.com/PureWriter/about-page/master/images/app_mosaic.png",
         "me.drakeet.puremosaic",
         "专注打码的轻应用，包含功能：传统马赛克、毛玻璃效果、选区和手指模式打码，更有创新型高亮打码和 LowPoly 风格马赛克。只为满足一个纯纯的打码需求，让打码也能成为一种赏心悦目。",
-        "https://www.coolapk.com/apk/me.drakeet.puremosaic",
+        "https://play.google.com/store/apps/details?id=me.drakeet.puremosaic",
         "2017-10-09 16:46:57",
         "2017-10-09 16:46:57", 2.64, true)
     );
